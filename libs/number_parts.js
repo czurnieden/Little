6b2cb89db3d5616c84bd55b29bf3429385055b9c
czurnieden <czurnieden@gmx.de>
2014-10-04 01:16:52 +0200

    Bugfixes, mainly bugfixes

--- libs/number_parts.js
+++ libs/number_parts.js
@@ -16,9 +16,9 @@ if(typeof Little === 'undefined'){
 Number.prototype.getSignBit = function(){
     "use asm";
     var high = 0 >>> 0;
-    double_int.setFloat64(0, x);
+    double_int.setFloat64(0, this);
     high = double_int.getUint32(4);
-    return (high & 0x80000000);
+    return (high & 0x80000000) >>> 31;
 };
 Number.prototype.getLowWord = function(){
     double_int.setFloat64(0, this);
@@ -26,16 +26,16 @@ Number.prototype.getLowWord = function(){
 };
 Number.prototype.setLowWord = function(an_uint32){
     double_int.setFloat64(0, this);
-    double_int.setUint32(0,an_uint32);
+    double_int.setUint32(4,an_uint32);
     this = double_int.getFloat64(0);
 };
 Number.prototype.getHighWord = function(){
-    double_int.setFloat64(4, this);
+    double_int.setFloat64(0, this);
     return double_int.getUint32(0);
 };
 Number.prototype.setHighWord = function(an_uint32){
     double_int.setFloat64(0, this);
-    double_int.setUint32(4,an_uint32);
+    double_int.setUint32(0,an_uint32);
     this = double_int.getFloat64(0);
 };
 Number.prototype.getExponent = function(){
