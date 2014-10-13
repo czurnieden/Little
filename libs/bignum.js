6d018c0919eefa7cbe203c5cb70ae083467fb7aa
czurnieden <czurnieden@gmx.de>
2014-10-14 01:44:31 +0200

    ADD: FFT multiplication and squaring

--- libs/bignum.js
+++ libs/bignum.js
@@ -126,13 +126,18 @@ var MP_NO = false;
 
 var KARATSUBA_MUL_CUTOFF = 125;
 var KARATSUBA_SQR_CUTOFF = 200;
+
+// although not tested yet, but seem reasonable
+var FFT_MUL_CUTOFF  = 2048;
+// do one karatsuba mul. if limit is reached
+// 2^23 is below limit and strill  8,388,608 limbs that is > 10^(10^14) 
+var FFT_UPPER_LIMIT = 1<<23; // only if NTT is not implemented
+var FFT_SQR_CUTOFF  = 2048;
+
+
 /*
 var TOOM_MUL_CUTOFF;
 var TOOM_SQR_CUTOFF;
-
-var FFT_MUL_CUTOFF;
-var FFT_UPPER_LIMIT; // only if NTT is not implemented
-var FFT_SQR_CUTOFF;
 
 var NTT_MUL_CUTOFF;
 var NTT_SQR_CUTOFF;
@@ -149,8 +154,15 @@ var TOOM_COOK_5_SQR_CO;
 var MP_DIGIT_BIT = 26;
 // Digit mask (e.g.: 0x3fffffff for 30 bit long digits)
 var MP_MASK = ((1<<MP_DIGIT_BIT)-1);
-// Too large a digit by one.
+// Too large a digit by one. (radix)
 var MP_DIGIT_MAX = (1<<MP_DIGIT_BIT);
+
+
+// half digits needed for FFT multiplication
+var MP_HALF_DIGIT_BIT  = (MP_DIGIT_BIT>>1)
+var MP_HALF_DIGIT      = (1<< MP_HALF_DIGIT_BIT )
+var MP_HALF_DIGIT_MASK = (MP_HALF_DIGIT-1)
+
 
 // Some limits regarding floating point size (unused, came for Tom Wu's stuff)
 // maximum usable bits in mantissa
@@ -163,6 +175,22 @@ var MP_FLOAT_DIGIT_DIFF = MP_FLOAT_BIT - MP_DIGIT_BIT;
 var MP_FLOAT_2_DIGIT_DIFF = 2 * MP_DIGIT_BIT - MP_FLOAT_BIT;
 
 Number.INT_MAX   =  9007199254740992; // 2^53
+
+/* Used in FFT code */
+/*
+  The size of the L1-cache in bytes. The number here is that of the data cache
+  part of an AMD Duron. The Linux kernel gives a lot of information e.g.:
+    grep . /sys/devices/system/cpu/cpu0/cache/index*//*
+  There is also lscpu(1) wich is easier to use.
+  On Windows:
+    http://msdn.microsoft.com/en-us/library/ms683194.aspx
+    http://www.cpuid.com/softwares/cpu-z.htm
+  Lack of access to a Mac leaves that part blank. The new MacOS is based on BSD,
+  so 'dmesg' might work or
+    cat /var/run/dmesg.boot | grep CPU
+ */
+if(typeof  MP_L1_SIZE === 'undefined')
+  var MP_L1_SIZE = 65536;
 
 // Memory for some of the bit-juggling below
 var double_int = new DataView(new ArrayBuffer(8));
@@ -1289,6 +1317,9 @@ Bigint.prototype.mul = function(bi){
      }
   */
   // check for cutoffs to do T-C or FFT respectively here
+  if(Math.min(a.used,b.used) >=  FFT_MUL_CUTOFF){
+    return a.fft_mul(b);
+  }
   if(Math.min(a.used,b.used) >= 2 * KARATSUBA_MUL_CUTOFF){
     return a.karatsuba(b);
   }
@@ -1348,6 +1379,10 @@ Bigint.prototype.square = function(){
 Bigint.prototype.sqr = function(){
   this.sign = MP_ZPOS;
 
+  if(this.used >= FFT_SQR_CUTOFF){
+    // FFT does both in one function
+    return this.fft_mul();
+  }
   if(this.used >= 2 * KARATSUBA_SQR_CUTOFF){
     return this.karatsuba_square();
   }
@@ -1732,7 +1767,7 @@ Bigint.prototype.divrem = function(bint){
   return [q,r];
 };
 
-
+// this function returns a bigint as the remainder.
 Bigint.prototype.divremInt = function(si){
   var divrem2in1 = function(u,m,v,q,B){
     var k = 0,t; 
@@ -1880,6 +1915,393 @@ Bigint.prototype.karatsuba_square = function(bint){
   xy = (x1y1.dlShift(2*m)).add(t1.dlShift(m)).add(x0y0);
   
   return xy;
+};
+
+
+Bigint.prototype.fft_mul = function(bint) {
+  /* base two integer logarithm */
+  var highbit = function(n) {
+    var r = 0 >>> 0;
+    var m = n >>> 0;
+    while (m >>>= 1) {
+      r++;
+    }
+    return r;
+  };
+  var bi_to_fft = function(a, fa, b, fb, len) {
+
+    var length_a, length_b, length_needed, i, j, hb;
+
+    /* Check of the multiplicands happens earlier */
+    length_a = a.used;
+    length_b = b.used;
+
+    /* Digits get split in half, so twice the length is needed*/
+    length_needed = (length_a + length_b) * 2;
+    /* final length must be a power of two to keep the FFTs simple */
+    hb = highbit(length_needed);
+    /* check for the rare case that it is already a power of 2 */
+    if (length_needed != 1 << hb) {
+      length_needed = 1 << (hb + 1);
+    }
+    /* Send computed length back to caller */
+    len[0] = length_needed;
+
+    /* Put splitted digits in double-array, in the same order as in mp_int */
+    for (i = 0, j = 0; i < length_needed / 2; i++, j += 2) {
+      if (i < length_a) {
+        fa[j] = 1.0 * (a.dp[i] & MP_DIGIT_MASK);
+        fa[j + 1] = 1.0 * ((a.dp[i] >>> MP_DIGIT_BIT_HALF) & MP_DIGIT_MASK);
+      }
+      /* padding a */
+      if (i >= length_a) {
+        fa[j] = 0.0;
+        fa[j + 1] = 0.0;
+      }
+      if (i < length_b) {
+        fb[j] = 1.0 * (b.dp[i] & MP_DIGIT_MASK);
+        fb[j + 1] = 1.0 * ((b.dp[i] >>> MP_DIGIT_BIT_HALF) & MP_DIGIT_MASK);
+      }
+      /* padding b */
+      if (i >= length_b) {
+        fb[j] = 0.0;
+        fb[j + 1] = 0.0;
+      }
+    }
+  };
+  /* same as dp_to_fft() for a single multiplicand for squaring */
+  var bi_to_fft_single = function(a, fa, len) {
+    var length_a, length_needed, i, j, hb;
+    length_a = a.used;
+    length_needed = (length_a * 2) * 2;
+    hb = highbit(length_needed);
+    if (length_needed != 1 << hb) {
+      length_needed = 1 << (hb + 1);
+    }
+    len[0] = length_needed;
+
+    for (i = 0, j = 0; i < length_needed / 2; i++, j += 2) {
+      if (i < length_a) {
+        fa[j] = 1.0(a.dp[i] & MP_DIGIT_MASK);
+        fa[j + 1] = 1.0((a.dp[i] >>> MP_DIGIT_BIT_HALF) & MP_DIGIT_MASK);
+      }
+      if (i >= length_a) {
+        fa[j] = 0.0;
+        fa[j + 1] = 0.0;
+      }
+    }
+    return MP_OKAY;
+  };
+  var fft_to_bi = function(fft_array, len) {
+    var new_length, i, j, e;
+    var carry, temp;
+    var a = new Bigint(0);
+
+    /* Result cannot exceed length/2, hence add two */
+    new_length = len;
+
+    /* The FFT multiplication does no carry (it's one of the tricks of it) */
+    carry = 0;
+    for (i = 0; i < len; i++) {
+      temp = carry;
+      carry = 0;
+      temp += Math.round(fft_array[i]);
+      if (temp >= MP_DIGIT_HALF) {
+        carry = Math.floor(temp / MP_DIGIT_HALF);
+        temp = temp % MP_DIGIT_HALF;
+      }
+      /* memory is still expensive, not a thing to waste easily */
+      fft_array[i] = temp;
+    }
+
+    /* re-marry the digits */
+    for (i = 0, j = 0; j < new_length; i++, j += 2) {
+      a.dp[i] = ((fft_array[j + 1])) & MP_DIGIT_MASK;
+      a.dp[i] <<= MP_DIGIT_BIT_HALF;
+      a.dp[i] |= ((fft_array[j])) & MP_DIGIT_MASK;
+      /* and count them all */
+      a.used++;
+    }
+    if (carry) {
+      a.dp[i] = carry;
+      a.used++;
+    }
+    a.clamp();
+    return a;
+  };
+  var fht_dif_iterative = function(x, n, offset, do_loop) {
+    var m = 0 >>> 0,
+      mh = 0 >>> 0,
+      mq = 0 >>> 0;
+    var i, j, k;
+    var a, b, t, c, s, u, v, tmp;
+    // a pointer to x in the original
+    var dp;
+    for (m = n; m > 1; m >>>= 1) {
+      mh = m >>> 1;
+      mq = mh >>> 1;
+      t = Math.PI / mh;
+      a = Math.sin(0.5 * t);
+      a *= 2.0 * a;
+      b = Math.sin(t);
+      for (i = 0; i < n; i += m) {
+        dp = offset + i; // dp = x + i in original
+        for (j = 0, k = mh; j < mh; ++j, ++k) {
+          u = x[dp + j];
+          v = x[dp + k];
+          x[dp + j] = u + v;
+          x[dp + k] = u - v;
+        }
+        dp += mh;
+        c = 1.0;
+        s = 0.0;
+        for (j = 1, k = mh - 1; j < mq; ++j, --k) {
+          tmp = c;
+          c -= a * c + b * s;
+          s -= a * s - b * tmp;
+          u = x[dp + j];
+          v = x[dp + k];
+          x[dp + j] = u * c + v * s;
+          x[dp + k] = u * s - v * c;
+        }
+      }
+      if (!do_loop) break;
+    }
+    return;
+  };
+  var fht_dif_rec = function(x, n, offset) {
+    var nh = 0 >>> 0;;
+    if (n == 1)
+      return;
+    if (n < Math.floor(MP_L1_SIZE / (2 * 8))) { // 8 = sizeof(double)
+      fht_dif_iterative(x, n, offset, true);
+      return;
+    }
+    fht_dif_iterative(x, n, offset, false);
+    nh = n >>> 1;
+    fht_dif_rec(x, nh, offset);
+    fht_dif_rec(x, nh, nh + offset);
+    return;
+  };
+  var fht_dit_iterative = function(x, n, offset, do_loop) {
+    var m = 0 >>> 0,
+      mh = 0 >>> 0,
+      mq = 0 >>> 0;
+    var i, j, k;
+    var a, b, t, u, v, c, s, tmp;
+    var dp;
+
+    m = (do_loop) ? 2 : n;
+    for (; m <= n; m <<= 1) {
+      mh = m >>> 1;
+      mq = mh >>> 1;
+      t = Math.PI / mh;
+      a = Math.sin(0.5 * t);
+      a *= 2.0 * a;
+      b = Math.sin(t);
+      for (i = 0; i < n; i += m) {
+        dp = offset + i + mh; // was dp = x + i + mh
+        c = 1.0;
+        s = 0.0;
+        for (j = 1, k = mh - 1; j < mq; ++j, --k) {
+          tmp = c;
+          c -= a * c + b * s;
+          s -= a * s - b * tmp;
+          u = x[dp + j];
+          v = x[dp + k];
+          x[dp + j] = u * c + v * s;
+          x[dp + k] = u * s - v * c;
+        }
+        dp -= mh;
+        for (j = 0, k = mh; j < mh; ++j, ++k) {
+          u = x[dp + j];
+          v = x[dp + k];
+          x[dp + j] = u + v;
+          x[dp + k] = u - v;
+        }
+      }
+    }
+    return;
+  };
+  var fht_dit_rec = function(x, n, offset) {
+    var nh;
+
+    if (n == 1)
+      return;
+    if (n < Math.floor(MP_L1_SIZE / (2 * 8))) { // 8 = sizeof(double)
+      fht_dit_iterative(x, n, offset, true);
+      return;
+    }
+    nh = n >>> 1;
+    fht_dit_rec(x, nh, offset);
+    fht_dit_rec(x, nh, nh + offset);
+    fht_dit_iterative(x, n, offset, false);
+    return;
+  };
+  /*
+    The FHT convolution from Jörg Arndt's book.
+    The code looks a bit messy but only on the face of it. This method avoids
+    the otherwise costly bit reversing (which is called "revbin" in J. Arndt's
+    book).
+  */
+  var fht_conv_core = function(f, g, n, v /*=0.0*/ ) {
+    var nh, r, rm, k, km, tr, m;
+    var xi, xj, yi, yj;
+    if (v == 0.0)
+      v = 1.0 / n;
+
+    g[0] *= (v * f[0]);
+    if (n >= 2)
+      g[1] *= (v * f[1]);
+    if (n < 4)
+      return;
+    v *= 0.5;
+    nh = (n >>> 1);
+    r = nh;
+    rm = n - 1;
+    xi = f[r];
+    xj = f[rm];
+    yi = g[r];
+    yj = g[rm];
+    g[r] = v * ((xi + xj) * yi + (xi - xj) * yj);
+    g[rm] = v * ((-xi + xj) * yi + (xi + xj) * yj);
+
+    k = 2;
+    km = n - 2;
+    while (k < nh) {
+      rm -= nh;
+      tr = r;
+      r ^= nh;
+      for (m = (nh >>> 1); !((r ^= m) & m); m >>>= 1) {;
+      }
+
+      xi = f[r];
+      xj = f[rm];
+      yi = g[r];
+      yj = g[rm];
+      g[r] = v * ((xi + xj) * yi + (xi - xj) * yj);
+      g[rm] = v * ((-xi + xj) * yi + (xi + xj) * yj);
+      --km;
+      ++k;
+      rm += (tr - r);
+      r += nh;
+      xi = f[r];
+      xj = f[rm];
+      yi = g[r];
+      yj = g[rm];
+      g[r] = v * ((xi + xj) * yi + (xi - xj) * yj);
+      g[rm] = v * ((-xi + xj) * yi + (xi + xj) * yj);
+      --km;
+      ++k;
+    }
+    return;
+  };
+  var fht_autoconv_core = function(f, n, v /*=0.0*/ ) {
+    var nh, r, rm, k, km, tr, m;
+    var xi, xj, xi2, xj2, xij;
+    if (v == 0.0)
+      v = 1.0 / n;
+
+    f[0] *= (v * f[0]);
+    if (n >= 2)
+      f[1] *= (v * f[1]);
+    if (n < 4)
+      return;
+    v *= 0.5;
+    nh = (n >>> 1);
+    r = nh;
+    rm = n - 1;
+    xi = f[r];
+    xj = f[rm];
+    xi2 = xi * xi;
+    xj2 = xj * xj;
+    xij = (2 * xi * xj);
+    f[r] = v * (xi2 + xij - xj2);
+    f[rm] = v * (-xi2 + xij + xj2);
+
+    k = 2;
+    km = n - 2;
+    while (k < nh) {
+      rm -= nh;
+      tr = r;
+      r ^= nh;
+      for (m = (nh >>> 1); !((r ^= m) & m); m >>>= 1) {;
+      }
+      xi = f[r];
+      xj = f[rm];
+      xi2 = xi * xi;
+      xj2 = xj * xj;
+      xij = (2 * xi * xj);
+      f[r] = v * (xi2 + xij - xj2)
+
+      f[rm] = v * (-xi2 + xij + xj2);
+
+      --km;
+      ++k;
+      rm += (tr - r);
+      r += nh;
+      xi = f[r];
+      xj = f[rm];
+      xi2 = xi * xi;
+      xj2 = xj * xj;
+      xij = (2 * xi * xj);
+      f[r] = v * (xi2 + xij - xj2);
+      f[rm] = v * (-xi2 + xij + xj2);
+      --km;
+      ++k;
+    }
+    return;
+  };
+
+  var MP_fft = function(x, y, len) {
+    var n;
+    n = len[0];
+    if (n < 2) return MP_VAL;
+    fht_dif_rec(x, n, 0);
+    fht_dif_rec(y, n, 0);
+    fht_conv_core(x, y, n, 0.0);
+    fht_dit_rec(y, n, 0), 0;
+    return MP_OKAY;
+  };
+  var MP_fft_sqr = function(x, len) {
+    var n;
+    n = len;
+    if (n < 2) return MP_VAL;
+    fht_dif_rec(x, n, 0);
+    fht_autoconv_core(x, n, 0.0);
+    fht_dit_rec(x, n, 0);
+    return MP_OKAY;
+  };
+  var fft_mul = function(a, b) {
+    var fa = [],
+      fb = [],
+      len = [];
+    var c;
+    // checks and balances
+
+    bi_to_fft(a, fa, b, fb, len);
+    MP_fft(fa, fb, len);
+
+    c = fft_to_bi(fb, len);
+    return c;
+  };
+  var fft_sqr = function(a) {
+    var fa = [],
+      len = [];
+    var c
+      // checks and balances
+
+    bi_to_fft_single(a, fa, len);
+    MP_fft_sqr(fa, len);
+
+    c = fft_to_bi(fa, len);
+    return c;
+  };
+
+  if(arguments.length == 1){
+    return fft_mul(this,bint);
+  }
+  return fft_sqr(this);
 };
 
 
